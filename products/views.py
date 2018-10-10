a2f1b115dbf8fb90cf421b230f1c026f4da80381
Dai Jones <mrdaijones@yahoo.com>
2018-10-10 16:27:01 -0600

    Getting there!

--- products/views.py
+++ products/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+#from .models import Products
+
+def home(request):
+    #products = Products.objects
+    #return render(request, 'products/home.html', {'products':products})
+    return render(request, 'products/home.html')
